9e70da37bd33aab6117d1b7426403d0a195882d0
Golla Anil <gollaanil013@gmail.com>
2025-10-28 23:17:58 +0530

    Jenkins file is added

--- test.java
+++ test.java
@@ -1,7 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Test step: running tests...");
-        // simple unit-check
         if (build.add(2, 3) != 5) {
             System.err.println("TEST FAILED: add(2,3) != 5");
             System.exit(1);
