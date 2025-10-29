6e16ff4b5247132b29b4c244e71461d1fa510182
Golla Anil <gollaanil013@gmail.com>
2025-10-29 12:27:35 +0530

    user nane jyosna

--- deploy.java
+++ deploy.java
@@ -1,5 +1,6 @@
 public class deploy {
     public static void main(String[] args) {
+        System.out.println("user name : "+" jysona ");
         System.out.println("Deploy step: performing deployment...");
         System.out.println("Deployment is happening .....");
         System.out.println("Deployment successful");
