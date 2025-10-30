1a58f7f97fea5b5a42aedc32b09f87581565fc68
Golla Anil <gollaanil013@gmail.com>
2025-10-30 22:30:39 +0530

    commit made to test the jenkins webhook

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Test step: running tests...");
+        System.out.println("Here is the test.java file ");
         if (build.add(2, 3) != 5) {
             System.err.println("TEST FAILED: add(2,3) != 5");
             System.exit(1);
