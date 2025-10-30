818a39c3d442ba4eff27c6ecd76b95cb93566bcf
Golla Anil <gollaanil013@gmail.com>
2025-10-30 22:02:41 +0530

    change made in build.java file

--- build.java
+++ build.java
@@ -1,10 +1,10 @@
 public class build {
-    // simple logic to be "built" and later tested
     public static int add(int a, int b) {
         return a + b;
     }
-
+    
     public static void main(String[] args) {
+        System.out.println("some thingg changed");
         System.out.println("Build step: sources ready");
     }
 }
